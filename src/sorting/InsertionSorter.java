d8d257f9e43c290dee91ab207fdeb93f27bbbeda
BAmine <amine.benhalloum@gmail.com>
2014-08-22 13:34:41 +0200

    Added reverse insertion sort

--- src/sorting/InsertionSorter.java
+++ src/sorting/InsertionSorter.java
@@ -17,4 +17,18 @@ public class InsertionSorter {
             A[i+1]=key;
         }
     }
+
+    public static void InsertionSortReverse(int[] A){
+        int key;
+        int i;
+        for(int j=1;j<A.length;j++){
+            key=A[j];
+            i=j-1;
+            while(i>=0 && A[i]<key){
+                A[i+1]=A[i];
+                i--;
+            }
+            A[i+1]=key;
+        }
+    }
 }
--- src/sorting/Main.java
+++ src/sorting/Main.java
@@ -8,6 +8,12 @@ public class Main {
         for(int i:A){
             System.out.print(i+" ");
         }
+        System.out.println("\n");
+        int[] B={10,1,3,4,5,9,8,2,1,7};
+        InsertionSorter.InsertionSortReverse(B);
+        for(int i:B){
+            System.out.print(i+" ");
+        }
 
     }
 }
