c36674bbd832a620f45b696ad018bf4aa016e6fa
BAmine <amine.benhalloum@gmail.com>
2014-08-27 11:32:53 +0200

    Random permutations of arrays by sorting

---
+++ src/randomized/ArrayRandomizer.java
@@ -0,0 +1,39 @@
+package randomized;
+
+import java.util.Random;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Amine
+ * Date: 27/08/14
+ * Time: 11:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArrayRandomizer {
+    public static void permuteBySorting(int[] A){
+        Random rnd=new Random();
+        int n=A.length;
+        int[] P=new int[n];
+        for(int i=0;i<n;i++){
+            P[i]=rnd.nextInt(n*n*n);
+        }
+        sortUsingArray(A,P);
+    }
+
+    private static void sortUsingArray(int[] a, int[] p) {
+        int key;
+        int key2;
+        int i;
+        for(int j=1;j<p.length;j++){
+            key=p[j];
+            key2=a[j];
+            i=j-1;
+            while(i>=0 && p[i]>key){
+                a[i+1]=a[i];
+                i--;
+            }
+            p[i+1]=key;
+            a[i+1]=key2;
+        }
+    }
+}
---
+++ src/randomized/Main.java
@@ -0,0 +1,23 @@
+package randomized;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Amine
+ * Date: 27/08/14
+ * Time: 11:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args){
+        int[] A={1,2,3,4,5,6,7,8,9};
+        ArrayRandomizer.permuteBySorting(A);
+        printList(A);
+    }
+
+    private static void printList(int[] a) {
+        for(int i: a){
+            System.out.print(i+" ");
+        }
+        System.out.println("\n");
+    }
+}
